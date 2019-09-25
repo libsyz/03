dfcd3d097cf7df82394eea304d7f46c8d149cc64
Miguel Jimenez <denkatalabeno@gmail.com>
2019-09-25 11:38:50 +0800

    implemented failing unit tests for post route

--- app/api.rb
+++ app/api.rb
@@ -1,10 +1,9 @@
 require 'sinatra/base'
 require 'json'
+require_relative 'ledger'
 
 module ExpenseTracker
-
   class API < Sinatra::Base
-
     def initialize(ledger)
       @ledger = ledger
       super
@@ -13,12 +12,17 @@ module ExpenseTracker
       # Need to check the params and pass them to the JSON generate
       expense = JSON.parse(request.body.read)
       result = @ledger.record(expense)
-      JSON.generate(expense_id: result.expense_id)
+
+      if result.success?
+        JSON.generate(expense_id: result.expense_id)
+      else
+        status 422
+        JSON.generate(error: result.error_message)
+      end
     end
 
     get '/expenses/:date' do
       JSON.generate([])
     end
-
   end
 end
--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -4,39 +4,53 @@ require 'rack/test'
 require 'pry'
 
 module ExpenseTracker
-  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
   RSpec.describe API do
     include Rack::Test::Methods
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+    let(:expense) { { "some" => "data" } }
 
     def app
       API.new(ledger)
+    end
+
+    before do
+      allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
     end
 
 
     describe 'POST expenses' do
       context 'when the expense is successfully recorded' do
         it 'returns the expense id' do
-          expense = { "some" => "data" }
-
-          allow(ledger).to receive(:record)
-            .with(expense)
-            .and_return(RecordResult.new(true, 417, nil))
-
-        post '/expenses', JSON.generate(expense)
-
-
-        parsed = JSON.parse(last_response.body)
-        expect(parsed).to include('expense_id' => 417)
+          post '/expenses', JSON.generate(expense)
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
         end
 
-        it 'responds with a 200 (OK)'
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
       context 'when the expense fails validation' do
-        it 'returns an error message'
-        it 'responds with a 422 (Unprocessable Entity'
+        let(:expense) { {"some" => "data"} }
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense).and_return(RecordResult.new(false, 417, "Expense Incomplete"))
+        end
+        it 'returns an error message' do
+          post "/expenses", JSON.generate(expense)
+          expect(last_response.body).to include("Expense Incomplete")
+        end
+
+        it 'responds with a 422 (Unprocessable Entity)' do
+          post "/expenses", JSON.generate(expense)
+          expect(last_response.status).to eq 422
+        end
       end
     end
   end
